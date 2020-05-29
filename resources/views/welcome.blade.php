e355d87aa63e80bfeab5f91accb1d5fa451c60e1
Antony <a.lapitski.developer@gmail.com>
2020-05-29 13:32:46 +0300

    Changed design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,6 +62,7 @@
                 margin-bottom: 30px;
             }
         </style>
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -83,7 +84,7 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
+                <div class="test"> </div>
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
